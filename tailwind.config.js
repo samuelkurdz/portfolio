3c88bf758adefa9201723625771f68dd2e1074ae
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-09-06 15:43:26 +0100

    changed file and tailwind structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 // const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['.src/pages/**/*.{js,ts,jsx,tsx}', '.src/components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     colors: {
