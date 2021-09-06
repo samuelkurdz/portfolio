7d1c8dc28aedec6b8c4e70bd31300d1132c5a539
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-09-06 19:07:20 +0100

    added socila media links

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -5,16 +5,16 @@
 // };
 
 function Avatar() {
-	return (
-		<div className="w-40 h-40 mx-auto rounded-full p-1 border-4 border-primary">
-			<img
-				src="/sam.jpg"
-				// style={imgStyle}
-				className="md:mx-auto rounded-full"
-				alt="me"
-			/>
-		</div>
-	);
+  return (
+    <div className="w-40 h-40 mx-auto rounded-full p-1 border-4 border-primary">
+      <img
+        src="/sam.jpg"
+        // style={imgStyle}
+        className="md:mx-auto rounded-full"
+        alt="me"
+      />
+    </div>
+  );
 }
 
 export default Avatar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,27 @@
 import Avatar from '../components/Avatar';
+import SocialMedia from '../components/SocialMedia';
 import ThemeSvg from '../components/ThemeSvg';
 
 function Homepage(): JSX.Element {
   return (
     <div className="mt-12 mx-auto md:w-10/12 w-10/12 text-center">
       <Avatar />
+      <ThemeSvg />
       <h1 className="mt-3 mb-6 font-black tracking-tight text-3xl lg:text-6xl text-primary">
         Samuel Adebayo
       </h1>
 
       <div className="space-y-3 text-xl">
-        <p>Hello, I’m a software engineer!</p>
+        <p>Hello, I’m a Software Engineer!</p>
         <p>I am currently building this Blog</p>
         <p>
           <span role="img" aria-label="builder and tools">
             👲🛠
           </span>
         </p>
+        {/* <a href="https://linkedin.com/in/sambayo">Linkedin</a> */}
+        <SocialMedia />
       </div>
-      <ThemeSvg />
     </div>
   );
 }
