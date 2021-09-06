7e80d9f6b95005c6b0922fd5d522ea6d5b251cac
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-09-06 17:20:57 +0100

    avatar added

---
+++ components/Avatar.tsx
@@ -0,0 +1,20 @@
+// import CSS from 'csstype';
+
+// const imgStyle: CSS.Properties = {
+// 	// filter: 'grayscale(1)',
+// };
+
+function Avatar() {
+	return (
+		<div className="w-40 h-40 mx-auto rounded-full p-1 border-4 border-primary">
+			<img
+				src="/sam.jpg"
+				// style={imgStyle}
+				className="md:mx-auto rounded-full"
+				alt="me"
+			/>
+		</div>
+	);
+}
+
+export default Avatar;
